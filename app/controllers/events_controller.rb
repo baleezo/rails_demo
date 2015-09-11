b4ac9cecb37d79104e1c9f43249cf5913be98208
baleezo <baleezo@teamt5.org>
2015-09-11 18:55:11 +0800

    [A] First commit

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,52 @@
+class EventsController < ApplicationController
+  
+  before_action :set_event, :only => ['show', 'edit', 'update', 'destroy']
+  
+  def index
+    @events = Event.all
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    #@event = Event.new(:name => params[:event][:name])
+    @event.save
+
+    redirect_to :action => :index
+  end
+
+  def show
+    @page_title = @event.name
+  end
+
+  def edit
+    @page_title = @event.name
+  end
+
+  def update
+    @page_title = @event.name
+    @event.update(event_params)
+
+    redirect_to :action => :show, :id => @event
+  end
+
+  def destroy
+    @event.destroy
+
+    redirect_to :action => :index
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :description)
+  end
+
+  def set_event
+    @event = Event.find(params[:id])
+  end
+
+end
